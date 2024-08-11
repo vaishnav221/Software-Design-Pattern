16aa33a08c76936099f4435da03f3b58c227f021
VAISHNAV S R <vaishnavsr221@gmail.com>
2024-08-12 02:32:50 +0530

    Day 9 Progress

---
+++ DayWiseProgress/Day 9/src/main/java/com/vaishnav/sdedvaralbackend/auth/AuthenticationService.java
@@ -0,0 +1,75 @@
+package com.vaishnav.sdedvaralbackend.auth;
+
+
+
+import com.vaishnav.sdedvaralbackend.config.JwtService;
+import com.vaishnav.sdedvaralbackend.model.Token;
+import com.vaishnav.sdedvaralbackend.model.User;
+import com.vaishnav.sdedvaralbackend.repo.TokenRepo;
+import com.vaishnav.sdedvaralbackend.repo.UserRepo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+
+    private final UserRepo userRepo;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+    private final TokenRepo tokenRepo;
+
+    public AuthenticationResponse register(RegisterRequest request) {
+        var user  = User.builder()
+                .name(request.getName())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(request.getRole())
+                .build();
+        userRepo.save(user);
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+        var user = userRepo.findByEmail(request.getEmail()).orElseThrow();
+        var jwtToken = jwtService.generateToken(user);
+        revokeAllUserTokens(user);
+        saveUserToken(user, jwtToken);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .role(user.getRole())
+                .build();
+
+    }
+
+    private void saveUserToken(User user, String accessToken) {
+        var token = Token.builder().user(user).token(accessToken).expired(false).revoked(false).build();
+        tokenRepo.save(token);
+    }
+
+    private void revokeAllUserTokens(User user) {
+        var validUserTokens = tokenRepo.findAllByUser_IdAndExpiredFalseAndRevokedFalse(user.getId());
+        if (validUserTokens.isEmpty())
+            return;
+        validUserTokens.forEach(token -> {
+            token.setExpired(true);
+            token.setRevoked(true);
+        });
+        tokenRepo.saveAll(validUserTokens);
+    }
+
+}
---
+++ DayWiseProgress/Day 9/src/main/java/com/vaishnav/sdedvaralbackend/auth/CustomLogoutHandler.java
@@ -0,0 +1,44 @@
+package com.vaishnav.sdedvaralbackend.auth;
+
+
+import com.vaishnav.sdedvaralbackend.config.JwtService;
+import com.vaishnav.sdedvaralbackend.repo.TokenRepo;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class CustomLogoutHandler implements LogoutHandler {
+
+    private final TokenRepo tokenRepo;
+    private final JwtService jwtService;
+
+    @Override
+    public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+
+        System.out.println("Logout Functionality Called");
+
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            return;
+        }
+
+        jwt = authHeader.substring(7);
+        String username = jwtService.extractUserName(jwt);
+
+        if (username != null) {
+            var token = tokenRepo.findByToken(jwt).orElse(null);
+            if (token != null) {
+                token.setExpired(true);
+                token.setRevoked(true);
+                tokenRepo.save(token);
+            }
+        }
+    }
+}
+
